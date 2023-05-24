62982e28dd93bd62aab5b57241dac0a63d2ae98a
tayfunetta <tayfunemrekahraman@gmail.com>
2023-05-24 23:49:39 +0300

    first commit

---
+++ pages/components/info.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Info() {
+    return (
+        <>
+            <h1>Info page</h1>
+            <Link href="/">go to home page</Link>
+        </>
+    )
+}
